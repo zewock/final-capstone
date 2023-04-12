d0ab947f5ad3ef2929908289fffe47aa6dec8c86
steven beatty <faststeve22@gmail.com>
2023-04-12 18:32:46 -0400

    Got the getAllForms Method Workinggit add -Agit add -Agit add -A!

--- dotnet/Capstone/DAO/ForumDao.cs
+++ dotnet/Capstone/DAO/ForumDao.cs
@@ -40,35 +40,35 @@ namespace Capstone.DAO
 
 
         // GetAllForums() - Retrieves all forums.
-        public ForumListDTO getAllForums(string userName, string tokenUserRole, int userId = 0)
+        public ForumListDTO getAllForums(string userName, string tokenUserRole, int userId)
         {
             List<ForumsArray> forumsList = new List<ForumsArray>();
 
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("SELECT forums.forum_id, forums.topic, forums.user_id, forums.create_date, users.username" +
-                                                "(CASE WHEN Forum_Favorites.user_id = forums.user_id THEN 1 ELSE 0 END) AS is_favorite, " +
+                SqlCommand cmd = new SqlCommand("SELECT forums.forum_id, forums.topic, forums.user_id, forums.create_date, users.username, " +
+                                                "(CASE WHEN forum_favorites.user_id = forums.user_id THEN 1 ELSE 0 END) AS is_favorite, " +
                                                 "MAX(CASE WHEN Forum_Mods.user_id = @user_id THEN 1 ELSE 0 END) AS is_moderator, " +
                                                 "(SELECT COUNT(*) FROM Post_Upvotes_Downvotes p WHERE p.forum_id = forums.forum_id AND p.create_date > DATEADD(day, -1, GETDATE()) AND p.is_upvoted = 1) AS upvoteswithin24hours, " +
                                                 "(SELECT COUNT(*) FROM Post_Upvotes_Downvotes p WHERE p.forum_id = forums.forum_id AND p.create_date > DATEADD(day, -1, GETDATE()) AND p.is_downvoted = 1) AS downvoteswithin24hours, " +
                                                 "(SELECT COUNT(*) FROM Post_Upvotes_Downvotes p WHERE p.forum_id = forums.forum_id AND p.is_upvoted = 1) AS totalupvotes, " +
-                                                "(SELECT COUNT(*) FROM Post_Upvotes_Downvotes p WHERE p.forum_id = forums.forum_id AND p.is_downvoted = 1) AS totaldownvotes FROM Forums " +
-                                                "LEFT JOIN Forum_Mods ON forums.forum_id = Forum_Mods.forum_id AND Forum_Mods.user_id = @user_id -- use parameter in JOIN condition " +
-                                                "LEFT JOIN Users ON Forums.user_id = Users.user_id " +
-                                                "LEFT JOIN Forum_Favorites ON forums.forum_id = Forum_Favorites.forum_id AND Forum_Favorites.user_id = @user_id " +
-                                                "WHERE forums.is_visible = 1 OR Forums.user_id = @user_id " +
-                                                "GROUP BY forums.forum_id, forums.user_id, forums.topic, forums.create_date, forums.is_visible, users.username;", conn);
+                                                "(SELECT COUNT(*) FROM Post_Upvotes_Downvotes p WHERE p.forum_id = forums.forum_id AND p.is_downvoted = 1) AS totaldownvotes FROM forums " +
+                                                "LEFT JOIN forum_mods ON forums.forum_id = forum_mods.forum_id AND forum_mods.user_id = @user_id " +
+                                                "LEFT JOIN users ON Forums.user_id = users.user_id " +
+                                                "LEFT JOIN forum_favorites ON forums.forum_id = forum_favorites.forum_id AND forum_favorites.user_id = @user_id " +
+                                                "WHERE forums.is_visible = 1 OR forums.user_id = @user_id " +
+                                                "GROUP BY forums.forum_id, forums.user_id, forums.topic, forums.create_date, forums.is_visible, users.username, forum_favorites.user_id;", conn);
                 cmd.Parameters.AddWithValue("@user_id", userId);
                 SqlDataReader reader = cmd.ExecuteReader();
-                if(reader.Read())
+                while(reader.Read())
                 {
                     ForumsArray forumsArray = new ForumsArray();
-                    forumsArray.ForumID = Convert.ToInt32(reader["forums.forum_id"]);
-                    forumsArray.Topic = Convert.ToString(reader["forums.topic"]);
-                    forumsArray.CreateDate = Convert.ToDateTime(reader["forums.create_date"]);
-                    forumsArray.OwnerID = Convert.ToInt32(reader["forums.user_id"]);
-                    forumsArray.OwnerUsername = Convert.ToString(reader["Users.username"]);
+                    forumsArray.ForumID = Convert.ToInt32(reader["forum_id"]);
+                    forumsArray.Topic = Convert.ToString(reader["topic"]);
+                    forumsArray.CreateDate = Convert.ToDateTime(reader["create_date"]);
+                    forumsArray.OwnerID = Convert.ToInt32(reader["user_id"]);
+                    forumsArray.OwnerUsername = Convert.ToString(reader["username"]);
                     forumsArray.TotalNumUpvotes = Convert.ToInt32(reader["totalupvotes"]);
                     forumsArray.TotalNumDownvotes = Convert.ToInt32(reader["totaldownvotes"]);
                     forumsArray.UpvotesLast24Hours = Convert.ToInt32(reader["upvoteswithin24hours"]);
