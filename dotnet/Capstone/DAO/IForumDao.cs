7b5c3be321b48135e2189a4d02fc738681b2300b
steven beatty <faststeve22@gmail.com>
2023-04-12 15:31:44 -0400

    Added getAllForums DAO Method and added it to the interface

--- dotnet/Capstone/DAO/IForumDao.cs
+++ dotnet/Capstone/DAO/IForumDao.cs
@@ -1,9 +1,12 @@
 ﻿using Capstone.Models.DatabaseModles;
+using Capstone.Models.OutgoingDTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.DAO
 {
     public interface IForumDao
     {
         public int CreateForum(Forum forum);
+        public ActionResult getAllForums(ForumListDTO forumListDto, int userId = 0);
     }
 }
