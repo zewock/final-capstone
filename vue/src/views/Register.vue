b5f04c4d92d1db1558f3066d8f98f280966bf296
Zachary Mccay <zsmccay@gmail.com>
2023-04-13 10:41:47 -0400

    login and register

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="register" class="hero is-fullheight has-text-centered">
-    <form @submit.prevent="register">
+  <div id="register" class="hero is-fullheight has-text-centered ">
+    <form @submit.prevent="register" class="box">
       <h1>Create Account</h1>
       <div role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
       <div class="form-input-group">
         <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
+        <input type="text" id="username" v-model="user.username" required autofocus class="inputBars"/>
       </div>
       <div class="form-input-group">
         <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" required />
+        <input type="password" id="password" v-model="user.password" required class="inputBars "/>
       </div>
       <div class="form-input-group">
         <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
+        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required class="inputBars"/>
       </div>
       <button class="button is-primary is-focused mx-4" type="submit">Create Account</button>
       <router-link class="button is-primary" v-bind:to="{ name: 'home' }">Back</router-link>
@@ -76,12 +76,31 @@ export default {
 
 <style scoped>
 .form-input-group {
-  margin-bottom: 1rem;
+  color: #000000;
+  align-items: center;
+   display: flex;
+    justify-content: flex-end;
+    padding: .2em;
 }
 label {
-  margin-right: 0.5rem;
+   padding: .5em 1em .5em 0;
+  flex: 1;
+  color: #000000;
 }
 #register{
   justify-content: center;
+  color: #000000;
+}
+.box {
+  background-color: #FAF3E3;
+  max-width: 30%;
+  min-width: 20%
+}
+.hero {
+ display: flex;
+ align-items: center;
+}
+.inputBars {
+   flex: 2;
 }
 </style>
