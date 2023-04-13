cee4aee0c17ef632f70e760d87a85941ed32f1a5
Zachary Mccay <zsmccay@gmail.com>
2023-04-13 11:05:48 -0400

    fixed forum

---
+++ vue/src/views/Forum.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container is-fullscreen">
+    <topbar />
+    <sidebarLeft/>
+    <sidebar-right />
+    <logo />
+    </section>
+</template>
+
+<script>
+import Topbar from '../components/Topbar.vue';
+import sidebarLeft from '../components/SideBarLeft.vue';
+import sidebarRight from '../components/SidebarRight.vue'
+import logo from '../components/logo.vue'
+export default {
+    name: "forum",
+ components: {
+    Topbar,
+    sidebarLeft,
+    sidebarRight,
+    logo
+  }
+}
+</script>
+
+<style scope>
+.container {
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  row-gap: 10px;
+  column-gap: 20px;
+  grid-template-areas: 
+  "logo nav nav"
+  "menu mainBody ad"
+  "menu mainBody ad";
+  height: 100vh;
+}
+@media screen and (min-width: 1408px) {
+.container:not(.is-max-desktop):not(.is-max-widescreen) {
+    max-width: 100%;
+    }
+}
+@media screen and (min-width: 1216px) {
+.container:not(.is-max-desktop) {
+    max-width: 100%;
+    }
+}
+@media screen and (min-width: 1024px) {
+.container {
+    max-width: 100%;
+    }
+}
+</style>
