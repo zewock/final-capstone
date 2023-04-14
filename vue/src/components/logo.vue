1f8eb896bd2f00e181ba5479b67f2054df90374b
Michael McGhen <mcghenmike@gmail.com>
2023-04-14 11:52:42 -0400

    Reformatted the layout to make it look cleaner when coding

--- vue/src/components/logo.vue
+++ vue/src/components/logo.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="logo, logo pl-5">
-      <img src='../components/logo.png'>
-    </div>
+  <div class="logo, logo pl-5">
+    <img src="../components/logo.png" />
+  </div>
 </template>
 
 <script>
 export default {
-data() {
-  return {
-    
-  }
-}
+  data() {
+    return {};
+  },
 };
 </script>
 <style scoped>
-.logo{
-    width: 120px;
-    position: fixed;
-    margin-top: 20px;
-    margin-left: 15px;
+.logo {
+  width: 120px;
+  position: fixed;
+  margin-top: 20px;
+  margin-left: 15px;
 }
 </style>
