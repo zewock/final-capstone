36a2b8c87ef2f7d3b5f1904cc6a5f2f0b3947a16
Michael McGhen <mcghenmike@gmail.com>
2023-04-14 10:42:22 -0400

    Added in the function to only get the date from CreateDate

--- vue/src/components/ForumList.vue
+++ vue/src/components/ForumList.vue
@@ -1,101 +1,95 @@
 <template>
   <body class="mainBody">
-      <section class="box" v-bind:key="form" v-show="form" >
-          <h1 style="">New Forum Form</h1>
+    <section class="box" v-bind:key="form" v-show="form" >
+        <h1 style="">New Forum Form</h1>
+        <div class="field">
+        <label class="label">Username</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        </div>
+        <div class="field">
+        <label class="label">Forum Name</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+            <span class="icon is-small is-left">
+              <i class="fa-sharp fa-light fa-input-text"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        </div>
+      <div class="field">
+        <label class="label">Topic</label>
+          <div class="control">
+            <div class="select">
+              <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+              </select>
+            </div>
+          </div>
+        </div>
 
-<div class="field">
-  <label class="label">Username</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-    <span class="icon is-small is-left">
-      <i class="fas fa-user"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-  </div>
-</div>
+        <div class="field">
+        <label class="label">Description</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+           </div>
+        </div>
 
-<div class="field">
-  <label class="label">Forum Name</label>
-  <div class="control has-icons-left has-icons-right">
-    <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-    <span class="icon is-small is-left">
-      <i class="fa-sharp fa-light fa-input-text"></i>
-    </span>
-    <span class="icon is-small is-right">
-      <i class="fas fa-check"></i>
-    </span>
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Topic</label>
-  <div class="control">
-    <div class="select">
-      <select>
-        <option>Select dropdown</option>
-        <option>With options</option>
-      </select>
-    </div>
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Description</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Textarea"></textarea>
-  </div>
-</div>
-
-<div class="field is-grouped">
-  <div class="control">
-    <button class="button" v-bind="form" @click="form = false" >Submit</button>
-  </div>
-  <div class="control">
-    <button class="button" v-bind="form" @click="form = false" >Cancel</button>
-  </div>
-</div>
-      </section>
-      
-        <div class="card">
-  <header class="card-header">
-    <section class="card-header-title">
-       <button class="button" v-bind="form" @click="form = true">New Forum</button>
-       <div class="dropdown is-hoverable">
-  <div class="dropdown-trigger">
-    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
-      <span>Options</span>
-      <span class="icon is-small">
-        <i class="fas fa-angle-down" aria-hidden="true"></i>
-      </span>
-    </button>
-  </div>
-  <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-    <div class="dropdown-content">
-      <div class="dropdown-item">
-          <button class="button">Moderators</button>
-          <button class="button">Users</button>
-          <button class="button">Delete</button>
-          
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button" v-bind="form" @click="form = false" >Submit</button>
+          </div>
+        <div class="control">
+          <button class="button" v-bind="form" @click="form = false" >Cancel</button>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
+    </section>
+      
+    <div class="card">
+        <header class="card-header">
+        <section class="card-header-title">
+          <button class="button" v-bind="form" @click="form = true">New Forum</button>
+        <div class="dropdown is-hoverable">
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
+            <span>Options</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+        <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+          <div class="dropdown-content">
+            <div class="dropdown-item">
+              <button class="button">Moderators</button>
+              <button class="button">Users</button>
+              <button class="button">Delete</button>    
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   </header>
 </div>
     <div class="card">
-  <header class="card-header" v-for="forum in forums" :key="forum.ForumID">
-    <p class="card-header-title"  >
-      {{forum.Topic}}
-    </p>
-    <button class="card-header-icon" aria-label="more options">
-     <span><time>{{createDate}}</time></span>
-    </button>
-  </header>
-</div>
-</body>
+      <header class="card-header" v-for="forum in formattedForums" :key="forum.ForumID">
+        <p class="card-header-title"  >
+          {{forum.Topic}}
+        </p>
+        <span class="date"><date>{{forum.FormattedCreateDate}}</date></span>
+      </header>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -105,13 +99,21 @@ export default {
   name: 'forumList',
   data() {
     return {
-        forums: []
+        forums: [],
+        form: false
     };
   },
   methods:{
     ViewForum(id){
       this.$router.push(`/forum/${id}`)
     },
+    formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  },
     
   },
   created() {
@@ -120,8 +122,22 @@ export default {
        const forumArray = parsedResponse.ForumArray;
        this.forums.push(...forumArray);
     });
+  },
+  computed: {
+  formattedForums() {
+    return this.forums.map(forum => {
+      const rawCreateDate = forum.CreateDate;
+      const formattedCreateDate = this.formatDate(rawCreateDate);
+      return {
+        ...forum,
+        FormattedCreateDate: formattedCreateDate
+      };
+    });
   }
+},
+
 };
+
 </script>
 
 <style scope>
@@ -134,12 +150,16 @@ export default {
   padding: 15px;
   border-radius:10px;
 }
+.card-header{
+  background-color: #FF9F29;
+  
+}
 .card-header-title{
   color:#1A4D2E;
   justify-content: space-between;
 }
 .card{
-  background-color: #FF9F29;
+  background-color: #1A4D2E;
   padding: 15px;
   margin-bottom: 10px;
   color: #000000;
@@ -165,5 +185,12 @@ body::-webkit-scrollbar {
     height: 100%;
     background-color: #FF9F29;
     z-index: 20;
+}
+.date{
+  color: black;
+  padding-right: 10px;
+}
+.card-header{
+  padding-top: 10px;
 }
 </style>
