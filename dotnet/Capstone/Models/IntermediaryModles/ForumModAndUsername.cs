95b1862129f0e6066fb7971f4d1955683385510d
Kevin Carson <kcarson9171787@outlook.com>
2023-04-14 18:22:17 -0400

    Get ForumsList working

---
+++ dotnet/Capstone/Models/IntermediaryModles/ForumModAndUsername.cs
@@ -0,0 +1,21 @@
+﻿namespace Capstone.Models.IntermediaryModles
+{
+    public class ForumModAndUsername
+    {
+        public int userId { get; set; }
+        public int forumId { get; set; }
+
+        public string username { get; set; }
+
+        public ForumModAndUsername()
+        {
+
+        }
+        public ForumModAndUsername(int userId, int forumId, string username)
+        {
+            this.userId = userId;
+            this.forumId = forumId;
+            this.username = username;
+        }
+    }
+}
