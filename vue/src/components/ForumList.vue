15f01b537eee7d4d7cbcfc04fa079d7d9880ccd8
Michael McGhen <mcghenmike@gmail.com>
2023-04-14 12:06:09 -0400

    one last thing

--- vue/src/components/ForumList.vue
+++ vue/src/components/ForumList.vue
@@ -139,7 +139,7 @@ export default {
       const year = date.getFullYear();
       const month = ("0" + (date.getMonth() + 1)).slice(-2);
       const day = ("0" + date.getDate()).slice(-2);
-      return `${day}-${month}-${year}`;
+      return `${month}-${day}-${year}`;
     },
   },
   created() {
