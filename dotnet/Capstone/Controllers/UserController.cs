651ca37e7dec662faee51e0a630a24dc88ba18ac
Kevin Carson <kcarson9171787@outlook.com>
2023-04-16 12:49:05 -0400

    Bann User/ Not let Banned Users Log in

--- dotnet/Capstone/Controllers/UserController.cs
+++ dotnet/Capstone/Controllers/UserController.cs
@@ -14,13 +14,45 @@ namespace Capstone.Controllers
 {
     [Route("")]
     [ApiController]
-    public class UserController
+    public class UserController : ControllerBase
     {
         private readonly IUserDao userDao;
 
         public UserController(IUserDao _userDao)
         {
             userDao = _userDao;
+        }
+
+        [HttpPut("/BanUser")]
+        public ActionResult BanUser(BanUserDTO banUserDTO)
+        {
+            int tokenUserId;
+            try
+            {
+                tokenUserId = userDao.GetUser(User.Identity.Name).UserId;
+            }
+            catch (Exception)
+            {
+                return StatusCode(401, "You need to be logged to bann a user");
+            }
+            
+            string tokenUserRole = userDao.GetUserRoleById(tokenUserId);
+            if (tokenUserRole != "admin")
+            {
+                return StatusCode(401, "Only admins can bann a user");
+            }
+
+            userDao.SetBanTime(banUserDTO);
+
+            if (banUserDTO.DeleteAllTraffic)
+            {
+                userDao.DeleteUsersContent(banUserDTO);
+                return StatusCode(200, "User was successfully banned, all users content was deleted");
+            }
+            else
+            {
+                return StatusCode(200, "User was successfully banned");
+            }
         }
     }
 }
--- dotnet/Capstone/DAO/UserSqlDao.cs
+++ dotnet/Capstone/DAO/UserSqlDao.cs
@@ -4,6 +4,7 @@ using Capstone.Models.IntermediaryModles;
 using Capstone.Models.DatabaseModles;
 using Capstone.Security;
 using Capstone.Security.Models;
+using Capstone.Models.IncomingDTOs;
 
 namespace Capstone.DAO
 {
@@ -26,7 +27,7 @@ namespace Capstone.DAO
                 {
                     conn.Open();
 
-                    SqlCommand cmd = new SqlCommand("SELECT user_id, username, password_hash, salt, user_role FROM users WHERE username = @username", conn);
+                    SqlCommand cmd = new SqlCommand("SELECT user_id, username, password_hash, salt, user_role, restore_ban_time FROM users WHERE username = @username", conn);
                     cmd.Parameters.AddWithValue("@username", username);
                     SqlDataReader reader = cmd.ExecuteReader();
 
@@ -74,6 +75,95 @@ namespace Capstone.DAO
             return username;
         }
 
+        public string GetUserRoleById(int userId)
+        {
+            string userRole = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT user_id, username, password_hash, salt, user_role FROM users WHERE user_id = @user_id", conn);
+                    cmd.Parameters.AddWithValue("@user_id", userId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        userRole = Convert.ToString(reader["user_role"]);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return userRole;
+        }
+
+        public void SetBanTime(BanUserDTO banUserDTO)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("update Users " +
+                        "Set restore_ban_time = @restore_ban_time " +
+                        "where user_id = @user_id;", conn);
+                    cmd.Parameters.AddWithValue("@user_id", banUserDTO.UserID);
+                    cmd.Parameters.AddWithValue("@restore_ban_time", banUserDTO.DateBanLifted);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+        }
+
+        public void DeleteUsersContent (BanUserDTO banUserDTO)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("update Forums " +
+                        "Set is_visible = 0 " +
+                        "where user_id = @user_id;", conn);
+                    cmd.Parameters.AddWithValue("@user_id", banUserDTO.UserID);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("update Forum_Posts " +
+                        "Set is_visible = 0 " +
+                        "where user_id = @user_id;", conn);
+                    cmd.Parameters.AddWithValue("@user_id", banUserDTO.UserID);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+        }
+
 
         public User AddUser(string username, string password, string role)
         {
@@ -90,7 +180,7 @@ namespace Capstone.DAO
                     cmd.Parameters.AddWithValue("@username", username);
                     cmd.Parameters.AddWithValue("@password_hash", hash.Password);
                     cmd.Parameters.AddWithValue("@salt", hash.Salt);
-                    cmd.Parameters.AddWithValue("@user_role", role);
+                    cmd.Parameters.AddWithValue("@user_role", "member");
                     cmd.ExecuteNonQuery();
                 }
             }
@@ -102,6 +192,8 @@ namespace Capstone.DAO
             return GetUser(username);
         }
 
+
+
         private User GetUserFromReader(SqlDataReader reader)
         {
             User u = new User()
@@ -111,6 +203,7 @@ namespace Capstone.DAO
                 PasswordHash = Convert.ToString(reader["password_hash"]),
                 Salt = Convert.ToString(reader["salt"]),
                 Role = Convert.ToString(reader["user_role"]),
+                RestoreBanDate = Convert.ToDateTime(reader["restore_ban_time"])
             };
 
             return u;
