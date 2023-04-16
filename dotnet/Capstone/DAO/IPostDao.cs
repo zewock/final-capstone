b246f2533835df5487f1123f08623fac8da38d86
steven beatty <faststeve22@gmail.com>
2023-04-16 10:18:11 -0400

    updated database and started working on searchPostsByKeyword method

--- dotnet/Capstone/DAO/IPostDao.cs
+++ dotnet/Capstone/DAO/IPostDao.cs
@@ -1,10 +1,12 @@
 ﻿using Capstone.Models.IntermediaryModles;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace Capstone.DAO
 {
     public interface IPostDao
     {
-        public List<ForumPostWithVotesAndUserName> GetPostsByForumId(int forumId);
+        public ActionResult<List<ForumPostWithVotesAndUserName>> GetPostsByForumId(int forumId);
+        public ActionResult<List<ForumPostWithVotesAndUserName>> SearchPostsForKeyword(string keyword);
     }
 }
