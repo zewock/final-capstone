8d88cab386f5df5bb7ff92c7647106ab6cb707b2
Michael McGhen <mcghenmike@gmail.com>
2023-04-16 15:32:26 -0400

    Re styled everything as well as fleshed out the post cards and prepared them to be able to look at comments

--- vue/src/components/ForumList.vue
+++ vue/src/components/ForumList.vue
@@ -119,25 +119,42 @@
       </div>
 
       <div class="card" v-else>
-        <div>
-          <header class="card-header input">
+        <div id="in-forum-title">
+          <header id="forum-title" class="card-header input">
             <section class="card-header-title">
-              {{ selectForum.title }}
-              {{ selectForum.description }}
+              <h1>{{ selectForum.title }}</h1>
+              <p>
+                {{ selectForum.description }}<br /><br />
+                @{{ selectForum.ownerUsername }}
+              </p>
             </section>
           </header>
+          <div class="post-card" v-for="post in postsList" :key="post.postId">
+            <button id="post-header" class="card-header button">
+              <h1 class="card-header-title">
+                {{ post.title }}
+              </h1>
+            </button>
+            <div class="card-content">
+              <div class="content">
+                <p>@{{ post.authorUserName }}</p>
+                {{ post.content }}
+                <br />
+                <br />
+                <time>{{ formatDateTime(post.createDate) }}</time>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item">Like | {{ post.upVotes }}</a>
+              <a href="#" class="card-footer-item"
+                >Dislike | {{ post.downVotes }}</a
+              >
+              <a href="#" class="card-footer-item">Favorite</a>
+            </footer>
+          </div>
         </div>
-        <header
-          class="card-header input"
-          v-for="post in postsList"
-          :key="post.postId"
-        >
-          <section class="card-header-title">
-            {{ post.title }}
-            {{ post.content }}
-          </section>
-        </header>
       </div>
+      
     </div>
   </body>
 </template>
@@ -185,6 +202,25 @@ export default {
         return "Invalid date";
       }
     },
+    formatDateTime(dateString) {
+      try {
+        const date = new Date(dateString);
+        if (isNaN(date)) {
+          throw new Error("Invalid date");
+        }
+        const year = date.getFullYear();
+        const month = ("0" + (date.getMonth() + 1)).slice(-2);
+        const day = ("0" + date.getDate()).slice(-2);
+        const hours = date.getHours();
+        const minutes = ("0" + date.getMinutes()).slice(-2);
+        const ampm = hours >= 12 ? "PM" : "AM";
+        const formattedHours = hours % 12 || 12;
+        return `${month}-${day}-${year} ${formattedHours}:${minutes} ${ampm}`;
+      } catch (error) {
+        console.error(`Error formatting date: ${error.message}`);
+        return "Invalid date";
+      }
+    },
 
     SaveForum() {
       ForumService.create(this.newForum).then((response) => {
@@ -204,7 +240,6 @@ export default {
       this.selectForum = forum;
       PostService.getPost(forum.forumID).then((response) => {
         this.postsList = response.data.value;
-    
       });
     },
   },
@@ -239,10 +274,54 @@ export default {
   padding: 15px;
   border-radius: 10px;
 }
+#in-forum-title #forum-title {
+  margin-bottom: 10px;
+}
+#in-forum-title .card-header {
+  background-color: #ff9f29; 
+  margin-bottom: 0;
+  border-radius: 10px;
+  position: sticky;
+  height: auto;
+  align-items: center;
+  width: 100%;
+}
+
+#in-forum-title .card-header h1 {
+  display: inline-flex;
+  color: #1a4d2e;
+  font-size: larger;
+  font-weight: bold;
+}
+#in-forum-title .card-header p {
+  width: 75%;
+  float: right;
+  font-size: smaller;
+  text-align: right;
+}
+
 .card-header {
   background-color: #ff9f29;
   margin-bottom: 10px;
   border-radius: 10px;
+  border-color: transparent;
+  height: auto;
+}
+.card-footer a{
+  color:#1a4d2e;
+}
+.card-footer a:hover {
+  background-color: #faf3e3;
+}
+.card-footer a:first-child:hover {
+  border-bottom-left-radius: 10px;
+}
+.card-footer a:last-child:hover {
+  border-bottom-right-radius: 10px;
+}
+.card-content {
+  background-color: #faf3e3;
+  color: #1a4d2e;
 }
 .card-header-title {
   color: #1a4d2e;
@@ -250,11 +329,18 @@ export default {
   display: flex;
   align-items: center;
 }
+.post-card {
+  background-color: #ff9f29;
+  border-radius: 10px;
+  margin-bottom: 10px;
+}
 .card {
   background-color: #1a4d2e;
   padding: 15px;
   margin-bottom: 10px;
   color: #000000;
+  border-radius: 10px;
+  z-index: 0;
 }
 .cards-container::-webkit-scrollbar {
   width: 0px; /* width of the entire scrollbar */
@@ -276,7 +362,9 @@ export default {
   right: 10px;
   align-items: center;
 }
-
+.dropdown-content {
+  z-index: 1;
+}
 .box {
   height: 100%;
   background-color: #ff9f29;
@@ -291,7 +379,7 @@ export default {
 }
 .cards-container {
   height: 100%;
-  overflow-y: auto; /* Enable vertical scrolling */
-  border-radius: 5px;
+  overflow-y: auto;
+  border-radius: 10px;
 }
 </style>
